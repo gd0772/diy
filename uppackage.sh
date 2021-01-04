e188109912f1b4db7d1628d68be178503ec0013e
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-01-05 07:21:35 +0800

    Update uppackage.sh

--- uppackage.sh
+++ uppackage.sh
@@ -43,6 +43,7 @@ git clone -b master --single-branch https://github.com/tty228/luci-app-servercha
 git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./package/diy/OpenAppFilter
 # echo '添加 smartdns'
 svn co https://github.com/gd0772/gd772-package/tree/main/smartdns ./feeds/packages/net/smartdns
+sed -i "s/192.168.1.1/192.168.123.3/$lan_ip/g" package/base-files/files/bin/config_generate
 # sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
 # sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
 ./scripts/feeds update -i
