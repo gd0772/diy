401a27611ebdf6cd75bcac28dccba79403d2891a
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-01-05 18:05:01 +0800

    Update uppackage.sh

--- uppackage.sh
+++ uppackage.sh
@@ -2,43 +2,32 @@
 #=================================================
 # Description: Build OpenWrt using GitHub Actions
 rm -rf ./package/lean/luci-theme-argon
-# echo '删除并替换luci-app-netdata'
+
 rm -rf ./package/lean/luci-app-netdata
 svn co https://github.com/gd0772/gd772-package/tree/main/luci-app-netdata ./package/lean/luci-app-netdata
-# echo '删除并替换netdata'
 rm -rf ./feeds/packages/admin/netdata
 svn co https://github.com/gd0772/gd772-package/tree/main/netdata ./feeds/packages/admin/netdata
-# echo '替换zzz-default-settings'
+
 curl -fsSL  https://raw.githubusercontent.com/gd0772/other/main/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
-# echo '添加关机'
 curl -fsSL  https://raw.githubusercontent.com/gd0772/other/main/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
-# echo '替换system.lua文件'
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
-# echo '修改 TTYD 名称'
+
 sed -i 's/ttyd/网页终端/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
-# echo '修改 服务菜单 里的app名称'
 sed -i 's/UPnP/UPnP设置/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
 sed -i 's/解锁网易云灰色歌曲/音乐解锁/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
-# echo '修改 网络存储 名称'
 sed -i 's/网络存储/存储/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
-# echo '修改 网络菜单 里的app名称'
 sed -i 's/Turbo ACC 网络加速/网络加速/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/Turbo ACC 网络加速/网络加速/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
-# echo '修改 带宽监控 名称'
 sed -i 's/带宽监控/统计/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 sed -i 's/invalid/# invalid/g' package/network/services/samba36/files/smb.conf.template
 sed -i 's/tables=1/tables=0/g' ./package/kernel/linux/files/sysctl-br-netfilter.conf
-# echo '添加 小猫咪'
+
 git clone -b master https://github.com/vernesong/OpenClash.git ./package/diy/OpenClash
-# echo '添加 passwall'
 git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/diy/passwall
-# echo '添加 HelloWorld'
-svn co https://github.com/jerrykuku/luci-app-vssr/trunk/ package/diy/luci-app-vssr
-# echo '添加 京东签到'
-svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/diy/luci-app-jd-dailybonus
-# echo '添加 应用过滤'
 git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./package/diy/OpenAppFilter
-# echo '添加 smartdns'
+
+svn co https://github.com/jerrykuku/luci-app-vssr/trunk/ package/diy/luci-app-vssr
+svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/diy/luci-app-jd-dailybonus
 svn co https://github.com/gd0772/gd772-package/tree/main/smartdns ./feeds/packages/net/smartdns
 sed -i "s/192.168.1.1/192.168.123.3/$lan_ip/g" package/base-files/files/bin/config_generate
 # sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
