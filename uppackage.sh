4c8f6b0d962796a25f6f5a0d5823af20125c22e0
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-01-05 18:20:06 +0800

    Update uppackage.sh

--- uppackage.sh
+++ uppackage.sh
@@ -4,15 +4,15 @@
 rm -rf ./package/lean/luci-theme-argon
 
 rm -rf ./package/lean/luci-app-netdata
-svn co https://github.com/gd0772/gd772-package/tree/main/luci-app-netdata ./package/lean/luci-app-netdata
+svn co https://github.com/gd0772/gd772-package/trunk/luci-app-netdata ./package/lean/luci-app-netdata
 rm -rf ./feeds/packages/admin/netdata
-svn co https://github.com/gd0772/gd772-package/tree/main/netdata ./feeds/packages/admin/netdata
+svn co https://github.com/gd0772/gd772-package/trunk/netdata ./feeds/packages/admin/netdata
 
 curl -fsSL  https://raw.githubusercontent.com/gd0772/other/main/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
 curl -fsSL  https://raw.githubusercontent.com/gd0772/other/main/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 
-sed -i 's/ttyd/网页终端/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
+sed -i 's/TTYD 终端/网页终端/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/UPnP/UPnP设置/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
 sed -i 's/解锁网易云灰色歌曲/音乐解锁/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
 sed -i 's/网络存储/存储/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
@@ -28,7 +28,7 @@ git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./
 
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/ package/diy/luci-app-vssr
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/diy/luci-app-jd-dailybonus
-svn co https://github.com/gd0772/gd772-package/tree/main/smartdns ./feeds/packages/net/smartdns
+svn co https://github.com/gd0772/gd772-package/trunk/smartdns ./feeds/packages/net/smartdns
 sed -i "s/192.168.1.1/192.168.123.3/$lan_ip/g" package/base-files/files/bin/config_generate
 # sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
 # sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
