3f86127f12c46691100efd2078c44273677ccbc1
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-01-09 14:31:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -40,7 +40,7 @@ git clone https://github.com/pymumu/openwrt-smartdns.git ./feeds/packages/net/sm
 svn co https://github.com/gd0772/diy/trunk/luci-app-netdata ./package/lean/luci-app-netdata
 svn co https://github.com/gd0772/diy/trunk/netdata ./feeds/packages/admin/netdata
 # echo '替换 分流助手 实现归类'
-curl - fsSL  https://raw.githubusercontent.com/gd0772/diy/main/mwan3helper_status.htm > ./package/lean/luci-app-mwan3helper/luasrc/view/mwan3helper/mwan3helper_status.htm
+curl -fsSL https://raw.githubusercontent.com/gd0772/diy/main/mwan3helper_status.htm > ./package/lean/luci-app-mwan3helper/luasrc/view/mwan3helper/mwan3helper_status.htm
 
 
 # echo '修改插件名称'
