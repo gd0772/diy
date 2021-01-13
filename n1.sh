bba5eee011809c394c68884588fd15f10aa8b08c
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-01-14 03:21:07 +0800

    Update n1.sh

--- n1.sh
+++ n1.sh
@@ -74,7 +74,6 @@ curl -fsSL https://raw.githubusercontent.com/gd0772/diy/main/mwan3helper_status.
 
 # echo '版本号更新'
 curl -fsSL  https://raw.githubusercontent.com/gd0772/other/main/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
-
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
-# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+# echo '更换内核'
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/armvirt/Makefile
 ./scripts/feeds update -i
