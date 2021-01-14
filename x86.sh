70435161719d679ddcee484db7613f48e403c82e
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-01-14 18:48:36 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -44,6 +44,10 @@ git clone https://github.com/pymumu/openwrt-smartdns.git ./feeds/packages/net/sm
 # echo '汉化实时监控'
 svn co https://github.com/gd0772/diy/trunk/luci-app-netdata ./package/lean/luci-app-netdata
 svn co https://github.com/gd0772/diy/trunk/netdata ./feeds/packages/admin/netdata
+# echo '替换USB打印'
+svn co https://github.com/gd0772/diy/trunk/public/luci-app-usb-printer ./package/lean/luci-app-usb-printer
+# echo '替换openvpn客户端'
+svn co https://github.com/gd0772/diy/trunk/public/luci-app-openvpn ./feeds/luci/applications/luci-app-openvpn
 
 # echo '修改插件名称'
 sed -i 's/TTYD 终端/网页终端/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
