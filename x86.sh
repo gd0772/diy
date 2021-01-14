41699545a8a2993dca2b0d7fff947d51668f996a
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-01-14 17:53:48 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 #=================================================
 # Description: Build OpenWrt using GitHub Actions
 # echo '删除重复多余主题'
@@ -70,11 +70,11 @@ sed -i 's/invalid/# invalid/g' package/lean/samba4/files/smb.conf.template
 sed -i 's/\"services\"/\"nas\"/g' package/lean/luci-app-samba4/luasrc/controller/samba4.lua
 # echo '移动 分流助手 到 网络菜单'
 sed -i 's/\"services\"/\"network\"/g' package/lean/luci-app-mwan3helper/luasrc/controller/mwan3helper.lua
-curl -fsSL https://raw.githubusercontent.com/gd0772/diy/main/mwan3helper_status.htm > ./package/lean/luci-app-mwan3helper/luasrc/view/mwan3helper/mwan3helper_status.htm
+curl -fsSL https://raw.githubusercontent.com/gd0772/diy/main/public/mwan3helper_status.htm > ./package/lean/luci-app-mwan3helper/luasrc/view/mwan3helper/mwan3helper_status.htm
 # echo '添加自定义防火墙说明'
-curl -fsSL  https://raw.githubusercontent.com/gd0772/diy/main/firewall.user > ./package/network/config/firewall/files/firewall.user
+curl -fsSL  https://raw.githubusercontent.com/gd0772/diy/main/public/firewall.user > ./package/network/config/firewall/files/firewall.user
 # echo '版本号更新'
-curl -fsSL  https://raw.githubusercontent.com/gd0772/other/main/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
+curl -fsSL  https://raw.githubusercontent.com/gd0772/diy/main/public/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
 # echo '更换内核'
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
