98a993c5eb3130a1c9cfc2f7473454b08aca4592
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-02-06 02:29:17 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -35,6 +35,8 @@ git clone https://github.com/Mattraks/helloworld.git ./package/diy/ssrplus
 git clone https://github.com/vernesong/OpenClash.git ./package/diy/OpenClash
 # echo '添加 Passwall'
 git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/diy/passwall
+# echo '添加 HelloWorld'
+git clone https://github.com/jerrykuku/luci-app-vssr.git ./package/diy
 # echo '添加 应用过滤'
 # git clone https://github.com/destan19/OpenAppFilter.git ./package/diy/OpenAppFilter
 # echo '添加 京东签到'
