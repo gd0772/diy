e7c7a37954bad233bc3e8914ca806967427398fc
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-03-04 00:35:40 +0800

    Update upgrade.sh

--- public/Custom/upgrade.sh
+++ public/Custom/upgrade.sh
@@ -80,7 +80,7 @@ Diy_Part2() {
 	echo "源码链接: ${REPO_URL}"
 	echo "源码分支: ${REPO_BRANCH}"
 	echo "源码作者: ${ZUOZHE}"
-	echo "机子型号: ${TARGET_PROFILE}"
+	echo "机型型号: ${TARGET_PROFILE}"
 	echo "安装需要的固件名称: ${Up_Firmware}"
 	echo "安装需要的固件后缀: ${Firmware_sfx}"
 	echo "自动更新固件版本: Firmware-${Openwrt_Version}"
