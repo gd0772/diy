53788da71b1029abffad524d1c84f3ddb43a7fac
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-03-04 03:26:08 +0800

    Update common.sh

--- public/Custom/common.sh
+++ public/Custom/common.sh
@@ -33,7 +33,7 @@ rm -rf ./*/*/{LICENSE,README,README.md}
 
 Diy_lean() {
 echo "LEAN源码自定义1"
-cp -Rf build/common/LEDE/* "${PATH1}"
+cp -Rf build/common/lean/* "${PATH1}"
 if [[ "${Modelfile}" == "Lean_x86_64" ]]; then
 sed -i '/IMAGES_GZIP/d' "${PATH1}/${CONFIG_FILE}" > /dev/null 2>&1
 echo -e "\nCONFIG_TARGET_IMAGES_GZIP=y" >> "${PATH1}/${CONFIG_FILE}"
