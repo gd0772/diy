a13d4185a39fddd3d861fe5692143c8719669083
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-03-06 21:09:02 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -50,7 +50,7 @@ svn co https://github.com/gd0772/diy/trunk/public/luci-app-usb-printer ./package
 # echo '添加 KPR去广告'
 git clone https://github.com/project-lede/luci-app-godproxy.git ./package/diy/luci-app-godproxy
 # echo '添加 KPR去广告规则数据'
-svn co https://github.com/gd0772/diy/trunk/public/rules ./package/diy/luci-app-godproxy/root/usr/share/koolproxy/data/rules
+#svn co https://github.com/gd0772/diy/trunk/public/rules ./package/diy/luci-app-godproxy/root/usr/share/koolproxy/data/rules
 
 # echo '修改插件名称'
 sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
